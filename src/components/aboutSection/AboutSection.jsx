d159c785b9f4c18d0784f87f6c6677ad91fb10e1
shehlaabdullayeva2004 <mi7ttwzlc@code.edu.az>
2025-09-01 23:57:34 +0400

    About section is created

--- src/components/aboutSection/AboutSection.jsx
+++ src/components/aboutSection/AboutSection.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import "./Style.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Style.scss";
 
-export default function HeroSection() {
+export default function AboutSec() {
   return (
-    <div>
-      
-    </div>
+    <section id="about-sec">
+      <div className="about-sec">
+        <div className="about-context row">
+          <div className="col-12 col-md-6">
+            <h2>Haqqımızda</h2>
+          </div>
+          <div className="right-side col-12 col-md-6">
+            <p>
+              MS ARCHITECTURAL MMC İN/L-2177/2016 lisenziya nömrəsi ilə 2016-cı
+              ildə təsis edilmişdir. Şirkət Azərbaycanın müxtəlif şəhər və
+              bölgələrində müasir standartlara cavab verən müxtəlif təyinatlı
+              layihələr həyata keçirmiş və memarlıq sahəsində öz mövqeyini
+              təsdiqləmişdir.
+            </p>
+          </div>
+          <button className="primary-button">
+            <Link>Daha Ətraflı</Link>
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Style.scss'
 import HeroSection from '../../components/heroSection/HeroSection';
+import AboutSec from '../../components/aboutSection/AboutSection';
 
 export default function Home() {
   return (
     <>
-      <HeroSection/>
+      <HeroSection />
+      <AboutSec/>
     </>
   );
 }
