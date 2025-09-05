9d92fbb24e664b018959072607243259941b580b
shehlaabdullayeva2004 <mi7ttwzlc@code.edu.az>
2025-09-05 14:32:56 +0400

    services

--- src/pages/servicesPage/Services.jsx
+++ src/pages/servicesPage/Services.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import AboutHeroSec from '../../components/aboutHeroSection/AboutHeroSec';
+import React from "react";
+import AboutHeroSec from "../../components/aboutHeroSection/AboutHeroSec";
 // import ServicesPageSec from '../../components/servicesPageSec/ServicesPage';
-import ServicesContainer from '../../components/ServicesContainer/ServicesContainer.jsx';
+import ServicesContainer from "../../components/servicesContainer/ServicesContainer.jsx";
 export default function ServicesPage() {
   return (
     <>
       <AboutHeroSec title={"Xidmətlər"} />
-      <ServicesContainer/>
-      
+      <ServicesContainer />
     </>
   );
 }
