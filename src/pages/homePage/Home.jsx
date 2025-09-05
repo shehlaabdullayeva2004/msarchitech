9f33183eac1da29eada9b13ef94aad2755fb05c1
shehlaabdullayeva2004 <mi7ttwzlc@code.edu.az>
2025-09-05 14:29:52 +0400

    problem is solved

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -4,7 +4,7 @@ import HeroSection from '../../components/heroSection/HeroSection';
 import AboutSec from '../../components/aboutSection/AboutSection';
 import Appreciation from '../../components/appreciations/Appreciation';
 import ServicesSection from '../../components/servicesSection/ServicesSection';
-import PurposeSection from '../../components/purposeSection/purposeSection';
+import PurposeSection from '../../components/purposeSection/PurposeSection';
 import ProjectsSec from '../../components/projectsSec/ProjectsSec';
 
 export default function Home() {
